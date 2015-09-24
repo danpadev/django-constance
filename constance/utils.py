8b0002d20996090264f82068b6f167a60a802a68
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2015-09-25 01:02:54 +0300

    Implemented choices support

--- constance/utils.py
+++ constance/utils.py
@@ -4,3 +4,17 @@ from django.utils.importlib import import_module
 def import_module_attr(path):
     package, module = path.rsplit('.', 1)
     return getattr(import_module(package), module)
+
+
+def get_default_item(value):
+    return value[0] if isinstance(value, tuple) else value
+
+
+def get_default_value(value):
+    item = get_default_item(value)
+    return item[0] if isinstance(item, tuple) else item
+
+
+def get_default_representation(value):
+    item = get_default_item(value)
+    return item[1] if isinstance(item, tuple) else item
